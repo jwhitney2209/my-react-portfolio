8c8f1c9f3e3cbed86a8011ae28e556c8b6070850
teachergg <jwhitney@bisd.us>
2022-06-26 05:30:20 -0500

    setup portfolio page

--- src/components/Portfolio/Portfolio.jsx
+++ src/components/Portfolio/Portfolio.jsx
@@ -9,6 +9,51 @@ const Portfolio = () => {
       <div className=''>
         <h2>Portfolio</h2>
       </div>
+      <div className='columns'>
+        <div className='column'>
+          <div className='card'>
+            <div className='card-header'>
+              <div className='card-header-title'>
+                Project #1
+              </div>
+            </div>
+            <div className='card-image'>
+              <figure className='image is-4by3'>
+              <img src="https://bulma.io/images/placeholders/1280x960.png" alt="Placeholder" /> 
+              </figure>
+            </div>
+            <div className='card-content'>
+              Description goes here...
+            </div>
+            <div className='card-footer'>
+              <a href="/" className='card-footer-item'>Github</a>
+              <a href="/" className='card-footer-item'>Deployed</a>
+            </div>
+          </div>
+        </div>
+        <div className='column'>
+          <div className='card'>
+            <div className='card-header'>
+              <div className='card-header-title'>
+                Project #2
+              </div>
+            </div>
+            <div className='card-image'>
+              <figure className='image is-4by3'>
+              <img src="https://bulma.io/images/placeholders/1280x960.png" alt="Placeholder" /> 
+              </figure>
+            </div>
+            <div className='card-content'>
+              Description goes here...
+            </div>
+            <div className='card-footer'>
+              <a href="/" className='card-footer-item'>Github</a>
+              <a href="/" className='card-footer-item'>Deployed</a>
+            </div>
+          </div>
+        </div>
+      </div>
+
     </div>
     </AnimatePage>
   )
